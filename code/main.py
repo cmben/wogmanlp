35d22a125115c2fe9e81b1f89d2a2166a68f1257
CHAITANYA <chaitanya.bendre@gmail.com>
2015-04-02 00:09:25 +0530

    Main Program now customizable

--- code/main.py
+++ code/main.py
@@ -15,9 +15,10 @@ if __name__=='__main__':
 		clean_review_text = clean.getAllTextInput(path)
 		assert(len(clean_review_text) > 0)
 		print 'Cleaning and Processing Complete....'
+		num_docs = input('Please enter number of documents you want to find topics for:')
 	
 		#get tf, idf matrix
-		td_dict, vocab = tfidf.tc(clean_review_text[:40])
+		td_dict, vocab = tfidf.tc(clean_review_text[:num_docs])
 		td = tfidf.to_sparse_matrix(td_dict, vocab).toarray()
 		idf = tfidf.to_vector(tfidf.idf_from_tc(td_dict), vocab)
 		tf_by_idf = tfidf.tf_mul_idf(td, idf)
@@ -31,7 +32,10 @@ if __name__=='__main__':
 		print 'Training data phase complete....'
 
 		#get the topic labels
-		topic_words = input('Please enter number of words for each topic:')
 		inv_vocab = tfidf.inverse_vocab(vocab)
 		label_plsa = pLSA(model)
-		print label_plsa.topic_labels(inv_vocab, topic_words)
+		print 'Time to play with the model.......Please enter 999 to abort'
+		topic_words = input('Please enter number of words for each topic:')
+		while topic_words != 999:
+				print label_plsa.topic_labels(inv_vocab, topic_words)
+				topic_words = input('Please enter number of words for each topic:')
